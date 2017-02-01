eb786714db253de7f03460875da8c3e139a1422d
Jair Ramos <j.irc.26@gmail.com>
2017-01-31 19:14:43 -0600

    Added roles in user migration file

--- db/migrate/20170109184645_create_users.rb
+++ db/migrate/20170109184645_create_users.rb
@@ -7,6 +7,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :email
       t.string :password_digest
       t.string :image
+      t.string :role
       t.timestamps
     end
   end

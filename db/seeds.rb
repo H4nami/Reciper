3f150e425da241b3d43c30c9c1381cc8d55ae180
Jair Ramos <j.irc.26@gmail.com>
2017-03-01 16:30:22 -0600

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+=begin
+u1 = User.create(name: "Garlic")
+u2 = User.create(name: "Carrot")
+u3 = User.create(name: "Broccoli")
+u4 = User.create(name: "Basil")
+u5 = User.create(name: "Thyme")
+u6 = User.create(name: "Portobello Mushrooms")
+u7 = User.create(name: "Parmesan Cheese")
+u8 = User.create(name: "Tomato")
+u9 = User.create(name: "Onion")
+u10 = User.create(name: "Serrano Pepper")
+=end
 
-i1 = Ingredient.create(name: "Garlic")
-i2 = Ingredient.create(name: "Carrot")
-i3 = Ingredient.create(name: "Broccoli")
-i4 = Ingredient.create(name: "Basil")
-i5 = Ingredient.create(name: "Thyme")
-i6 = Ingredient.create(name: "Portobello Mushrooms")
-i7 = Ingredient.create(name: "Parmesan Cheese")
-i8 = Ingredient.create(name: "Tomato")
-i9 = Ingredient.create(name: "Onion")
-i10 = Ingredient.create(name: "Serrano Pepper")
+=begin
+r1 = Recipe.create(name: "Garlic")
+r2 = Recipe.create(name: "Carrot")
+r3 = Recipe.create(name: "Broccoli")
+r4 = Recipe.create(name: "Basil")
+r5 = Recipe.create(name: "Thyme")
+r6 = Recipe.create(name: "Portobello Mushrooms")
+r7 = Recipe.create(name: "Parmesan Cheese")
+r8 = Recipe.create(name: "Tomato")
+r9 = Recipe.create(name: "Onion")
+r10 = Recipe.create(name: "Serrano Pepper")
+=end 
 
 =begin
 m1 = Meal.create(title: "", description: "", image "")
@@ -30,3 +44,28 @@ m8 = Meal.create(title: "", description: "", image "")
 m9 = Meal.create(title: "", description: "", image "")
 m10 = Meal.create(title: "", description: "", image "")
 =end
+
+i1 = Ingredient.create(name: "Garlic")
+i2 = Ingredient.create(name: "Carrot")
+i3 = Ingredient.create(name: "Broccoli")
+i4 = Ingredient.create(name: "Basil")
+i5 = Ingredient.create(name: "Thyme")
+i6 = Ingredient.create(name: "Portobello Mushrooms")
+i7 = Ingredient.create(name: "Parmesan Cheese")
+i8 = Ingredient.create(name: "Tomato")
+i9 = Ingredient.create(name: "Onion")
+i10 = Ingredient.create(name: "Serrano Pepper")
+
+=begin
+q1 = Quantity.create(name: "Garlic")
+q2 = Quantity.create(name: "Carrot")
+q3 = Quantity.create(name: "Broccoli")
+q4 = Quantity.create(name: "Basil")
+q5 = Quantity.create(name: "Thyme")
+q6 = Quantity.create(name: "Portobello Mushrooms")
+q7 = Quantity.create(name: "Parmesan Cheese")
+q8 = Quantity.create(name: "Tomato")
+q9 = Quantity.create(name: "Onion")
+q10 = Quantity.create(name: "Serrano Pepper")
+=end
+
